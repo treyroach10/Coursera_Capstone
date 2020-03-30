b55843bab349662583fa5827a5188782bcaaefcd
treyroach10 <62671783+treyroach10@users.noreply.github.com>
2020-03-30 18:19:02 -0500

    Add files via upload

---
+++ Coursera_Capstone.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+mkdir ~/Desktop/projects
+
+
+# In[2]:
+
+
+cd ~/Desktop/projects
+
+
+# In[3]:
+
+
+get_ipython().system('git clone https://github.com/treyroach10/Coursera_Capstone.git')
+
+
+# In[4]:
+
+
+ls
+
+
+# In[5]:
+
+
+cd ~/Desktop/projects/Coursera_Capstone
+
+
+# This notebook will be used for my Coursera Capstone project
+
+# In[6]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
